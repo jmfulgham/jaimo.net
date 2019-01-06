64226dc07dba9250523aeeb7da7a16963f062083
Mone't Fulgham <monetfulgham@gmail.com>
2019-01-06 14:35:58 -0500

    Update landing page form and layout

--- src/components/Views/Landing.js
+++ src/components/Views/Landing.js
@@ -1,42 +1,70 @@
 import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import './form.css';
+require("typeface-vidaloka");
 
+const style = {
+    container: {
+        padding: "1rem"
+    },
+
+    none: {
+        display: "none"
+    },
+    subhead: {
+         fontFamily: "Vidaloka"
+    },
+
+    email: {
+        color: 'primary'
+    }
+}
 export default class Landing extends Component{
     constructor(props){
         super(props)
-        this.state= [
-            {
+        this.state= {
             firstName: null,
-            email: null   
-            }
-        ]
+            email: null 
+        }
     }
+
+
 
     render(){
         return(
             <div className="landingForm"> 
-               <Typography variant="title">
-            For more information about courses or to request a consultation, fill out the form below.
+               <Typography variant="headline">
+                   For class announcements, freebies, and other tutorials, subscribe below.
             </Typography>
-            <form>
-             
-            <input
-            name= "first-name"
-            placeholder="First Name"
-            // onChange={this.handleChange}
-            />
-           
-            <input 
-            name = "email"
-            placeholder="Email"
-            // onChange={this.handleChange}
-            />
-             <input
-            type= "submit"
-            name="Submit"/>
-            </form>
-            </div>
+                <Typography variant="subheading">
+                    To work with me, contact <a href="mailto:mo@jaimo.net?Subject=Hi Mo, let's work together!" style={style.email}>mo@jaimo.net</a>
+                </Typography>
+                    <div id="mc_embed_signup">
+                        <form action="https://jaimo.us7.list-manage.com/subscribe/post?u=761012afcaf3bda37d7881e87&amp;id=bb54c439c5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+                            <div id="mc_embed_signup_scroll">
+
+                                <div className="mc-field-group">
+                                    <Typography style={style.subhead} variant='body2' color="primary"><label htmlFor="mce-FNAME">First Name* </label></Typography>
+                                    <input type="text" name="FNAME" id="mce-FNAME" required ></input>
+                                </div>
+                                <div>
+                                    <Typography style={style.subhead} variant='body2' color="primary"><label htmlFor="mce-EMAIL">Email Address*
+                                    </label></Typography>
+                                    <input type="email" name="EMAIL"  id="mce-EMAIL" required></input>
+                                </div>
+                                <div className="indicates-required"><Typography variant="caption">* indicates required</Typography></div>
+                                </div>	<div id="mce-responses" >
+                                <div className="response" id="mce-error-response" style={style.none}></div>
+                                <div className="response" id="mce-success-response" style={style.none}></div>
+                            </div>
+                            <div aria-hidden="true" style={style.none}><input type="text" name="b_761012afcaf3bda37d7881e87_bb54c439c5" tabIndex="-1"></input></div>
+                            <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"></input></div></form>
+                            </div>
+                    </div>
+
+
+
+
         )
     }
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Typography from "@material-ui/core/Typography";
 import Landing from '../Views/Landing';
 
@@ -7,25 +7,43 @@ require("typeface-vidaloka");
 require("typeface-quicksand");
 
 const styles = {
-  fontFamily: "Vidaloka",
-  marginTop: "8rem"
-};
+    text: {
+        fontFamily: "Vidaloka"
+    },
+
+    container: {
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'center',
+    },
+
+    child:{
+    padding: '9rem'
+    }
+
+}
 
 class Welcome extends Component {
-  render() {
-    return <div>
-        <Typography variant="display4" color="primary" style={styles} align="center">
-          JAI MONE'T
-        </Typography>
-
-        <Typography variant="body2" color="secondary" align="center" style={{ fontSize: "40px", fontFamily: "Quicksand" }}>
-          Software Engineer and Mobile App Developer
-        </Typography>
-        <div style ={{textAlign: 'center'}}>
-        <Landing />
-        </div>
-      </div>;
-  }
+    render() {
+        return <div style={styles.container}>
+            <div style={styles.child}>
+                <div>
+            <Typography variant="display4" color="primary" style={styles.text} align="center">
+                JAI MONE'T
+            </Typography>
+            </div>
+            <div>
+            <Typography variant="body2" color="secondary" align="center"
+                        style={{fontSize: "40px", fontFamily: "Quicksand"}}>
+                Software Engineer and Mobile App Developer
+            </Typography>
+            </div>
+            <div style={{textAlign: 'center'}}>
+                <Landing/>
+            </div>
+            </div>
+        </div>;
+    }
 }
 
 export default Welcome;
