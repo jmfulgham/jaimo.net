93702aeeadfcd41a4f41356926f6133bb3861757
Mone't Fulgham <monetfulgham@gmail.com>
2020-04-03 12:27:15 -0400

    Set up <Media/>

---
+++ src/components/Media/Media.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import Typography from "@material-ui/core/Typography";
+
+require("typeface-vidaloka");
+require("typeface-quicksand");
+
+const Media = () => {
+    return (<div className='parent'>
+        <Typography variant='display3'>Media</Typography>
+    </div>)
+
+};
+
+export default Media;
