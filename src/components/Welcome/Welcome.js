003d9b7a871511414fc80a3dc1ac05e89b6632d8
Mone't Fulgham <monetfulgham@gmail.com>
2020-04-05 16:58:04 -0400

    Update styles and copy

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -13,7 +13,7 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         maxWidth: '100%',
-        padding: '2rem',
+        backgroundColor: '#ffffff',
 
     },
 
@@ -25,28 +25,31 @@ const styles = {
     nav: {
         order: '1',
         alignSelf: 'center',
-        fontSize: "2.85vw"
+        fontSize: "2.85vw",
+        paddingTop: '5vh',
+        fontWeight: 'unset'
     },
 
     body: {
         width: '68%',
         alignSelf: 'center',
         order: '0',
-        fontSize: '4.4vw'
+        fontSize: '4.4vw',
+        padding: '2rem',
 
     }
 };
 
 const Welcome = () => {
 
-    return (<div className="container" style={styles.container}>
+    return (<div className="welcome-container" style={styles.container}>
             <div className="body" style={styles.body}>
 
 
                 Hello, my name is <text style={styles.titleFont}>
                 Mone't.<br/></text>
                 I'm a software engineer that turns <text style={styles.titleFont}>business logic</text> into
-                <text style={styles.titleFont}> responsive, scalable </text>applications.
+                <text style={styles.titleFont}> responsive, scalable </text>applications!
 
                 <div className="nav" style={styles.nav} aria-label="Navigation">
                     <NavLinks/>
