65efff27315255564c5ed0b561224d56a415f387
Mone't Fulgham <monetfulgham@gmail.com>
2020-04-05 16:58:50 -0400

    Update styles, add more copy and update layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const profilePicture = require('../../assets/JM BW.JPG');
-
 require("typeface-vidaloka");
 
 
@@ -9,11 +8,13 @@ const styles = {
     container: {
         display: 'flex',
         flexFlow: 'row wrap',
-        maxWidth: '3000px',
         justifyContent: 'space-evenly',
-        backgroundColor: '#9e9e9e',
         padding: '15px 0 25px 0',
         margin: '45px 0 45px 0',
+        borderStyle: 'solid',
+        borderColor:'#C4CCAB',
+        borderWidth: '5px 0 5px 0',
+        backgroundColor: '#ffffff'
     },
     h2: {
         color: '#673445',
@@ -22,38 +23,86 @@ const styles = {
 
     imgContainer: {
         order: '0',
+        alignSelf: 'center'
+    },
+
+    body: {
+        fontFamily: "Quicksand"
     },
 
     img: {
-        borderRadius: '65%'
+        borderRadius: '65%',
+        border: '1px solid #C4CCAB'
     },
-    bio:{
+    bio: {
         width: '55%',
         order: '1',
-        fontSize: '30px',
+        fontSize: '25px',
         alignSelf: 'center',
-        color: '#ffffff',
+    },
+    parent: {
+        marginTop: '15vh',
+        display: 'flex',
+        flexFlow: 'row wrap',
+        justifyContent: 'center',
+    },
+
+    title: {
+        fontFamily: 'Vidaloka',
+    },
+
+    section: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexFlow: 'row wrap'
+    },
+    tech: {
+        order: '0'
+    },
+    design: {
+        order: '1'
     }
 };
 
 const About = () => {
-    return (
-        <div className="about-container" style={styles.container}>
+    return (<div className="parent" style={styles.parent}>
+            <div className="about-container" style={styles.container}>
+                <div className="img-container" style={styles.imgContainer}>
+                    <img style={styles.img} src={profilePicture} alt="JM Selfie"/>
+                </div>
+                <div className="bio" style={styles.bio}>
+                    <h2 style={styles.h2}>About</h2>
+                    <text style={styles.body}> I am a software engineer with a background in economics,
+                        finance, and business analysis. Leveraging my experience, I turn business logic into responsive
+                        and scalable applications.
+                        I also create <a href="/graphics" alt="Jai Mone't Graphics">graphics</a>,
+                        and study design in my spare time.
+                    </text>
+                    <section className="tools" style={styles.section}>
 
-            <div className="img-container" style={styles.imgContainer}>
-                <img style={styles.img} src={profilePicture} alt="JM Selfie"/>
+                        <div className="technologies" style={styles.tech}>
+
+                            <h3>Development</h3>
+                            <ul>
+                                <li>React</li>
+                                <li>React Native</li>
+                                <li>Node.js</li>
+                                <li>GraphQL</li>
+                                <li>Kubernetes</li>
+                                <li>Docker</li>
+                            </ul>
+                        </div>
+                        <div className="design" style={styles.design}>
+                            <h3>Web/Graphic Design</h3>
+                            <ul>
+                                <li>Adobe Illustrator</li>
+                                <li>Adobe Photoshop</li>
+                                <li>Plain pencil and paper</li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
             </div>
-            <div className="bio" style={styles.bio}>
-                <h2 style={styles.h2}>Hey, I'm Mone't, but you can call me Mo...</h2>
-              Here is my bio, and I'm trying to see something about spacing
-                Here is my bio, and I'm trying to see something about spacing
-                Here is my bio, and I'm trying to see something about spacing
-                Here is my bio, and I'm trying to see something about spacing
-                Here is my bio, and I'm trying to see something about spacing
-                Here is my bio, and I'm trying to see something about spacing
-                Here is my bio, and I'm trying to see something about spacing
-
-        </div>
         </div>
     )
 };
