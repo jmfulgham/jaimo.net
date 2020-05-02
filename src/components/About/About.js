35a04a589c7b85b019ac1abb99e817928761474a
Mone't Fulgham <monetfulgham@gmail.com>
2020-05-02 15:26:05 -0400

    Figure out some spacing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,109 +1,132 @@
 import React from "react";
 
 const profilePicture = require('../../assets/JM BW.JPG');
-require("typeface-vidaloka");
 
 
 const styles = {
     container: {
         display: 'flex',
-        flexFlow: 'row wrap',
-        justifyContent: 'space-evenly',
-        padding: '15px 0 25px 0',
-        margin: '45px 0 45px 0',
-        borderStyle: 'solid',
-        borderColor:'#C4CCAB',
-        borderWidth: '5px 0 5px 0',
-        backgroundColor: '#ffffff'
-    },
-    h2: {
-        color: '#673445',
-        fontFamily: 'Vidaloka',
-    },
-
-    imgContainer: {
+        // flexFlow: 'row wrap',
+        // justifyContent: 'space-evenly',
+        // padding: '15px 0 25px 0',
+        margin: '35px 0 35px 0',
+        // backgroundColor: '#ffffff',
+        // height: '55vh',
         order: '0',
-        alignSelf: 'center'
+        maxWidth: '65vw'
+    },
+    text: {
+        // width: '25%',
+        alignSelf: 'flex-end'
+    },
+    second: {
+        display: 'flex',
+        flexFlow: 'row wrap',
+        order: '1',
+        marginTop: '15vh',
+    },
+    imgContainer: {
+        order: '2',
+        alignSelf: 'flex-end'
     },
 
     body: {
-        fontFamily: "Quicksand"
+        fontFamily: "Quicksand",
+        margin: '15px'
     },
 
     img: {
-        borderRadius: '65%',
-        border: '1px solid #C4CCAB'
+        border: '1px solid #C4CCAB',
+
     },
     bio: {
-        width: '55%',
+        // width: '65%',
         order: '1',
         fontSize: '25px',
-        alignSelf: 'center',
+        alignSelf: 'flex-end',
+        textAlign: 'end',
+        margin: '2vw'
     },
     parent: {
         marginTop: '15vh',
         display: 'flex',
-        flexFlow: 'row wrap',
         justifyContent: 'center',
     },
 
     title: {
         fontFamily: 'Vidaloka',
+        color: '#673445',
+        fontSize: '10vh',
     },
 
     section: {
         display: 'flex',
-        justifyContent: 'space-around',
-        flexFlow: 'row wrap'
+        order: '0',
+        alignItems: 'flex-end',
     },
     tech: {
-        order: '0'
+        order: '0',
+        width: '10vw',
+        height: '10vw'
     },
     design: {
-        order: '1'
+        order: '1',
+        width: '10vw',
+        height: '10vw'
     }
 };
 
 const About = () => {
-    return (<div className="parent" style={styles.parent}>
-            <div className="about-container" style={styles.container}>
-                <div className="img-container" style={styles.imgContainer}>
-                    <img style={styles.img} src={profilePicture} alt="JM Selfie"/>
-                </div>
-                <div className="bio" style={styles.bio}>
-                    <h2 style={styles.h2}>About</h2>
-                    <text style={styles.body}> I am a software engineer with a background in economics,
-                        finance, and business analysis. Leveraging my experience, I turn business logic into responsive
-                        and scalable applications.
-                        I also create <a href="/graphics" alt="Jai Mone't Graphics">graphics</a>,
-                        and study design in my spare time.
-                    </text>
-                    <section className="tools" style={styles.section}>
-
-                        <div className="technologies" style={styles.tech}>
-
-                            <h3>Development</h3>
-                            <ul>
-                                <li>React</li>
-                                <li>React Native</li>
-                                <li>Node.js</li>
-                                <li>GraphQL</li>
-                                <li>Kubernetes</li>
-                                <li>Docker</li>
-                            </ul>
-                        </div>
-                        <div className="design" style={styles.design}>
-                            <h3>Web/Graphic Design</h3>
-                            <ul>
-                                <li>Adobe Illustrator</li>
-                                <li>Adobe Photoshop</li>
-                                <li>Plain pencil and paper</li>
-                            </ul>
-                        </div>
-                    </section>
+    return (
+        <>
+            <div className="parent" style={styles.parent}>
+                <div className="about-container" style={styles.container}>
+                    <div className="img-container" style={styles.imgContainer}>
+                        <img style={styles.img} src={profilePicture} alt="JM Selfie"/>
+                    </div>
+                    <div className="bio" style={styles.bio}>
+                        <h1 style={styles.title}>Hey, I'm Mone't</h1>
+                        <section className="text" style={styles.text}>
+                            <text style={styles.body}> I am a software engineer
+                                with a background in economics,
+                                finance, and business analysis.
+                                Leveraging my experience,
+                                I turn business logic into responsive
+                                and scalable applications.
+                                I also create <a href="/graphics" alt="Jai Mone't Graphics">graphics</a>,
+                                and dabble in design
+                                in my spare time.
+                            </text>
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className='step-parent' style={styles.second}>
+                <div className="tools" style={styles.section}>
+
+                    <div className="technologies" style={styles.tech}>
+
+                        <h3>Development</h3>
+                        <ul>
+                            <li>React</li>
+                            <li>React Native</li>
+                            <li>Node.js</li>
+                            <li>GraphQL</li>
+                            <li>Kubernetes</li>
+                            <li>Docker</li>
+                        </ul>
+                    </div>
+                    <div className="design" style={styles.design}>
+                        <h3>Web/Graphic Design</h3>
+                        <ul>
+                            <li>Adobe Illustrator</li>
+                            <li>Adobe Photoshop</li>
+                            <li>Plain pencil and paper</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 };
 
