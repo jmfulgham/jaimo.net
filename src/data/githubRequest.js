423399f3cb6b67608bfa3ee9505b9fecb99aad66
Mone't Fulgham <monetfulgham@gmail.com>
2024-06-25 16:18:05 -0600

    Add Projects section and GH API call

---
+++ src/data/githubRequest.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const query = `
+    query {
+        user(login: "jmfulgham"){
+            pinnedItems(first: 4, types: REPOSITORY){
+                nodes {
+                    ...on Repository {
+                        name
+                        description
+                        primaryLanguage {
+                            name
+                        }
+                        url
+                    }
+                }
+            }
+        }
+}`
+
+export const getGithubData = async () => {
+    try {
+        const response = await fetch("https://api.github.com/graphql", {
+            method: "POST",
+            headers: {
+                Authorization: `bearer ${process.env.REACT_APP_GH_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({query})
+        })
+        const {data} = await response.json()
+        return data.user.pinnedItems.nodes
+    } catch (e) {
+        console.error(`Error getting projects, ${e}`);
+    }
+}
